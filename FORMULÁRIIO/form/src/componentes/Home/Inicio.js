74abf442ffc1270235df674f89551fbc30ee0a23
Nilton Cesar Jacintho <nilton.jacintho@gmail.com>
2023-09-13 17:14:44 -0300

    ajuste de grid

--- FORMULÁRIIO/form/src/componentes/Home/Inicio.js
+++ FORMULÁRIIO/form/src/componentes/Home/Inicio.js
@@ -3,30 +3,73 @@ import './estilo/Inicio.css'
 import { DataTable } from 'primereact/datatable';
 import { Column } from 'primereact/column';
 import { Button } from 'primereact/button';
-        
-
+import React, { useState, useEffect } from 'react';
+import axios from "axios"
 
 export default function Inicio() {
+  const [usuarioAtual, setUsuarioAtual] = useState({});
+
+  // useEffect(() => {
+  //   // Update the document title using the browser API
+  //   this.usuarioAtual = {};
+  // });
+
+
 const dadosProntos = useLoaderData()
-const botaoDelete = () =>{
-  return <Button icon="pi pi-times" rounded severity="danger" aria-label="Cancel" />
+const rowSelect = (row) =>{
+  console.log('=======================', row.data.id, row.data);
+  setUsuarioAtual(row.data)
+}
+const botaoDelete = (rowData) =>{
+  return <Button onClick={() => 
+    deleteUser(rowData)} icon="pi pi-times" rounded severity="danger" aria-label="Cancel"   />
 }
 
+  const deleteUser = (data) => {
+  console.log('ON DELETE ',data);
+   axios.delete(`http://localhost:4000/usuarios/${data.id}`)
+   .then(res =>{
+    // console.log("AAAAAAAAAAAAAAAAA")
+   })
+  //  .catch(err => console.log("erro"))
+  
+}
+
+  
+
+ 
 const botaoAlterar = () =>{
   return <Button icon="pi pi-search" severity="success" aria-label="Search" />
 }
 
-return (
+  return (
+    
   <div className="card">
-      <DataTable value={dadosProntos} scrollable scrollHeight="70vh"  tableStyle={{ minWidth: '50rem' }}>
+      <h1>{ usuarioAtual.nome}</h1>
+      <DataTable value={dadosProntos} scrollable scrollHeight="70vh"  
+      onRowClick={rowSelect}
+      selectionMode={'single'}
+      tableStyle={{ minWidth: '50rem' }}>
           <Column field="nome" header="Nome"></Column>
           <Column field="sobrenome" header="Sobrenome"></Column>
           <Column field="email" header="email"></Column>
           <Column field="genero" header="genero" ></Column>
-          <Column field="acoes" header="Ações" body={botaoDelete } style={{ minWidth: '2rem' }} ></Column>
+          <Column  header="Ações" body={botaoDelete} style={{ minWidth: '2rem' }} ></Column>
       </DataTable>
   </div>
 );
+
+
+  
+}
+
+
+
+export const dadosLoader = async () =>{
+   const res = await fetch ('http://localhost:4000/usuarios')
+   return res.json()
+}
+
 
 // //   return (
 // //     <div className='tabela'>
@@ -55,10 +98,3 @@ return (
 // //   </fieldset>
 // // </table>
 //     </div>
-  
-}
-
-export const dadosLoader = async () =>{
-   const res = await fetch ('http://localhost:4000/usuarios')
-   return res.json()
-}
